4387a668fc82a5537e0e039a0d89c5f67a4c35fa
Cezary Siwoń <siwon.cezary@gmail.com>
2024-03-23 19:51:00 +0100

    PATCH

---
+++ WebApi/Program.cs
@@ -0,0 +1,35 @@
+using ApplicationCore.Commons.Repository;
+using ApplicationCore.Interfaces.AdminService;
+using ApplicationCore.Interfaces.UserService;
+using ApplicationCore.Models;
+using ApplicationCore.Models.QuizAggregate;
+using Infrastructure.Memory;
+using Infrastructure.Memory.Repositories;
+using Web;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddTransient<IntGenerator>();
+builder.Services.AddSingleton<IGenericRepository<Quiz, int>, MemoryGenericRepository<Quiz, int>>();
+builder.Services.AddSingleton<IGenericRepository<QuizItem, int>, MemoryGenericRepository<QuizItem, int>>();
+builder.Services.AddSingleton<IGenericRepository<QuizItemUserAnswer, string>, MemoryGenericRepository<QuizItemUserAnswer, string>>();
+builder.Services.AddSingleton<IQuizUserService, QuizUserService>();
+builder.Services.AddSingleton<IQuizAdminService, QuizAdminService>();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.MapControllers();
+app.Seed();
+app.Run();
